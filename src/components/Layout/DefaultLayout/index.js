c7d3c4491555dc1b8c23416f606b6492270dd904
TonThanhTam <tamtt1512@gmail.com>
2023-04-08 17:39:12 +0700

    Xây dựng UI phần Header #1

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,14 +1,16 @@
-import clsx from 'clsx';
+import className from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
 import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
+
+const cx = className.bind(styles);
 function DefaultLayout({ children }) {
   return (
-    <div className={clsx(styles.wrapper)}>
+    <div className={cx('wrapper')}>
       <Header />
-      <div className={clsx(styles.container)}>
+      <div className={cx('container')}>
         <Sidebar />
-        <div className={clsx(styles.content)}>{children}</div>
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,10 +1,44 @@
 // Header được sài chung cho 2 layout (Default và Only), cách này giúp giải quyết vần đề trùng lặp code
 import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import {
+  faCircleXmark,
+  faSpinner,
+  faMagnifyingGlass,
+  faEllipsisVertical,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
 function Header() {
   return (
     <header className={clsx(styles.wrapper)}>
-      <div className={clsx(styles.inner)}>Header</div>
+      <div className={clsx(styles.inner)}>
+        <img src={images.logo} alt="Tiktok"></img>
+        <div className={clsx(styles.search)}>
+          <input placeholder="Tìm kiếm tài khoản và video" spellCheck={false} />
+          <button className={clsx(styles.clear)}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={clsx(styles.loading)} icon={faSpinner} />
+          <button className={clsx(styles.searchBtn)}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+          <div className={clsx(styles.inputBorder)}></div>
+        </div>
+        <div className={clsx(styles.actions)}>
+          <div>
+            <a href="/upload">
+              <div className={clsx(styles.upload)}>
+                <FontAwesomeIcon className={clsx(styles.uploadIcon)} icon={faPlus} />
+                <span className={clsx(styles.uploadSpan)}>Tải lên</span>
+              </div>
+            </a>
+          </div>
+          <button className={clsx(styles.loginBtn)}>Đăng nhập</button>
+          <FontAwesomeIcon className={clsx(styles.moreMenuIcon)} icon={faEllipsisVertical} />
+        </div>
+      </div>
     </header>
   );
 }
